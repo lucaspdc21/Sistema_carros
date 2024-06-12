eac7935cb7308cc0d2fa4eb68292d264dab8e13e
lucaspdc21 <lucaspdc21@gmail.com>
2024-06-12 11:06:31 -0300

    create Car Entity

--- cars/models.py
+++ cars/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Car (models.Model):
+    id = models.AutoField(primary_key=True)
+    model = models.CharField(max_length=150)
+    manufacturer = models.CharField(max_length=100)
+    model_year = models.IntegerField(blank=True, null=True)
+    factory_year = models.IntegerField(blank=True, null=True)
+    value = models.IntegerField(blank=True, null=True)
