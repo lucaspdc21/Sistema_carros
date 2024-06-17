93e360bb0d1e9e622cb1ab4653cf509ad8f1282b
lucaspdc21 <lucaspdc21@gmail.com>
2024-06-17 13:49:01 -0300

    modified:   cars/__pycache__/admin.cpython-312.pyc 	modified:   cars/__pycache__/models.cpython-312.pyc 	modified:   cars/admin.py 	new file:   cars/migrations/0002_manufacturer.py 	new file:   cars/migrations/0003_alter_car_manufacturer.py 	new file:   cars/migrations/__pycache__/0002_manufacturer.cpython-312.pyc 	new file:   cars/migrations/__pycache__/0003_alter_car_manufacturer.cpython-312.pyc 	modified:   cars/models.py 	modified:   db.sqlite3

--- cars/admin.py
+++ cars/admin.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
 
-from cars.models import Car
+from cars.models import Car, Manufacturer
 
 class CarAdmin (admin.ModelAdmin):
     list_display = ("model","manufacturer", "model_year", "value")
     search_fields = ("model", "model_year")
 
+class ManufacturerAdmin (admin.ModelAdmin):
+    list_display = ("name",)
+    search_fields = ("name",)
 
 admin.site.register(Car,CarAdmin)
+admin.site.register(Manufacturer ,ManufacturerAdmin)
--- cars/models.py
+++ cars/models.py
@@ -4,10 +4,13 @@ class Manufacturer (models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=150)
 
+    def __str__(self):
+        return self.name
+
 class Car (models.Model):
     id = models.AutoField(primary_key=True)
     model = models.CharField(max_length=150)
-    manufacturer = models.CharField(max_length=100)
+    manufacturer = models.ForeignKey(Manufacturer, on_delete=models.PROTECT, related_name="car_manufacturer")
     model_year = models.IntegerField(blank=True, null=True)
     factory_year = models.IntegerField(blank=True, null=True)
     value = models.IntegerField(blank=True, null=True)
