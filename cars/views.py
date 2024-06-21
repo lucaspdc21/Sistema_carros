4b16bbd62dc29bb9223268fa24f7d5023f7883e8
lucaspdc21 <lucaspdc21@gmail.com>
2024-06-21 11:08:07 -0300

    configuração da pagina de vizualização de veículos

--- cars/views.py
+++ cars/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
-# Create your views here.
+from cars.models import Car
+
+def cars_view (request):
+    cars = Car.objects.all()
+    search = request.GET.get('search')
+
+    if search:
+        #icontains ignora letras maiúsculas e minúsculas
+        cars = Car.objects.filter(model__icontains=search)
+
+    print(cars)
+    return render(request, 'cars.html', {'cars' : cars})
